edc699a72644e1925921211a9625abb6bed9b033
Amirhossein Shahraki <amirhossein.shahraki@protonmail.com>
2024-01-06 20:31:20 +0330

    SharedUIModule added to auth appliation, login page initiated

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedUIModule } from '@personal-task-manager/shared-ui';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, SharedUIModule],
 })
 export class AuthModule {}
